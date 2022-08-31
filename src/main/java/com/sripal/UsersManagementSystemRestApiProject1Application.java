a8737aee38f07719f6a7bed619941f56b1eb9665
Sripalyadav <kookatlasripalyadav@gmail.com>
2022-08-31 12:13:32 +0530

    hello

---
+++ src/main/java/com/sripal/UsersManagementSystemRestApiProject1Application.java
@@ -0,0 +1,13 @@
+package com.sripal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersManagementSystemRestApiProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersManagementSystemRestApiProject1Application.class, args);
+	}
+
+}
